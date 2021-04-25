42cdef967f0d2faa666f334a079789152effd987
Sudheer Singh <sudheersid0710@gmail.com>
2021-04-25 12:49:34 +0530

    added text and styles in HomePage

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -6,7 +6,13 @@ const HomePage = () => {
             <header className="content">
                 <h1 className="content-text">
                     Hi I am 
-                    <span>Sudheer Singh</span>
+                    <span> Sudheer Singh</span>
+                    <p className="content-sub-text">
+                        Web Developer And Competetive Pragrammer
+                    </p>
+                    <div className="icons">
+                         
+                    </div>
                 </h1>
             </header>
         </div>
