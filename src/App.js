97c9a276781bfa7d6efc249ac358a3280bde965b
Sudheer Singh <sudheersid0710@gmail.com>
2021-04-27 23:42:04 +0530

    separate components for routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import SideBar from './components/sidebar/SideBar';
-import HomePage from './components/home/HomePage';
-import {Route , Switch} from 'react-router-dom';
-import About from './components/about/About';
-import Contact from './components/contact/Contact';
-import Projects from './components/projects/Projects';
 import Animation from './assets/gif/animation.gif';
+import Routes from './components/routes/Routes';
 import './App.scss';
 
 const App = () => {
@@ -28,19 +23,8 @@ const App = () => {
 
         :
 
-        <div>
-        <div className="sideBar">
-          <SideBar />
-        </div>
-        <div className="mainBody">
-        <Switch>
-          <Route exact path="/" component={() => <HomePage/>} />
-          <Route exact path="/About" component={() => <About/> } />
-          <Route exact path="/Projects" component={() => <Projects/> } />
-          <Route exact path="/Contact" component={() => <Contact/> } />
-        </Switch>
-        </div>
-        </div>
+        <Routes />
+        
       }
     </div>
   );
---
+++ src/components/routes/Routes.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import SideBar from '../sidebar/SideBar';
+import HomePage from '../home/HomePage';
+import {Route , Switch} from 'react-router-dom';
+import About from '../about/About';
+import Contact from '../contact/Contact';
+import Projects from '../projects/Projects';
+
+const Routes = () => {
+    return(
+        <div>
+            <div className="sideBar">
+            <SideBar />
+            </div>
+            <div className="mainBody">
+            <Switch>
+              <Route exact path="/" component={() => <HomePage/>} />
+              <Route exact path="/About" component={() => <About/> } />
+              <Route exact path="/Projects" component={() => <Projects/> } />
+              <Route exact path="/Contact" component={() => <Contact/> } />
+            </Switch>
+        </div>
+        </div>
+    );
+}
+
+export default Routes;
