e92de65fd574d7a866eea3767f5cba0f4b91c001
Sudheer Singh <sudheersid0710@gmail.com>
2021-05-02 13:31:26 +0530

    ...

--- src/components/about/ImageDiv.js
+++ src/components/about/ImageDiv.js
@@ -2,8 +2,17 @@ import React from 'react';
 
 const ImageDiv = () => {
     return (
-        <div>
-            
+        <div className="image-div">
+            <div className="img">
+                <img src={about} alt="about" />
+            </div>
+            <div className="about-imfo">
+                <h4>I am <span> Sudheer Singh </span></h4>
+                <p>ksjbgk ksajdbi askdvjb j jd kljasdhbv lakdjbvlkj lhdbfldkb lKDJFBLDK LKDJ LKDBFLlkjsd lkajsbfl</p>
+                <div className="about-details"> 
+                    
+                </div>
+            </div>
         </div>
     )
 }
