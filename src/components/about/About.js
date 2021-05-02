a77353eab5d8df831f343bd237f5ec41b28e0b8f
Sudheer Singh <sudheersid0710@gmail.com>
2021-05-02 13:30:56 +0530

    imported ImageDIv in about component

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Title from './Title';
+import ImageDiv from './ImageDiv';
 
 const About  = () => {
     return(
         <div>
             <Title title={"About Me"} span={"About me"} />
+            <ImageDiv />
         </div>
     );
 }
