9656c4ec618820baab108d9e4d000b6cbd0c45a1
Sudheer Singh <sudheersid0710@gmail.com>
2021-05-02 13:41:17 +0530

    imported png in ImageDiv component

--- src/components/about/ImageDiv.js
+++ src/components/about/ImageDiv.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import SudheerSingh from '../../assets/images/sudheerSingh.PNG';
 
 const ImageDiv = () => {
     return (
         <div className="image-div">
             <div className="img">
-                <img src={about} alt="about" />
+                <img src={SudheerSingh} alt="about" />
             </div>
             <div className="about-imfo">
                 <h4>I am <span> Sudheer Singh </span></h4>
                 <p>ksjbgk ksajdbi askdvjb j jd kljasdhbv lakdjbvlkj lhdbfldkb lKDJFBLDK LKDJ LKDBFLlkjsd lkajsbfl</p>
                 <div className="about-details"> 
-                    
+
                 </div>
             </div>
         </div>
