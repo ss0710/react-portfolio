25566ba9e9a59d81aac188a8a40125ecff5705d8
Sudheer Singh <sudheersid0710@gmail.com>
2021-05-03 16:28:51 +0530

    created input feilds

--- src/components/contact/Form.js
+++ src/components/contact/Form.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React from 'react';
 
 const Form = () => {
     return (
-        <div>
-            form
-        </div>
+            <div className="form-section">
+                <div className="form-div">
+                    <h4><span>Get In Touch</span></h4>
+                    <form>      
+                    <input type="text" placeholder="Name" /><br/>
+                    <input type="email" placeholder="Email" /><br/>
+                    <input type="text" placeholder="Subject" /><br/>
+                    <textarea type="text" placeholder="Message"/>
+                    </form>
+                </div>
+                <div className="img-div">
+                    <div className="img-inner-div">
+                    </div>
+                </div>
+            </div>
     )
 }
 
