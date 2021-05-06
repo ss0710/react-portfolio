b5df2935a44392c71d03a761005c17485a3a8257
Sudheer Singh <sudheersid0710@gmail.com>
2021-05-06 17:32:42 +0530

    added certificate links

--- src/components/about/Achievements.js
+++ src/components/about/Achievements.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import LinkIcon from '@material-ui/icons/Link';
 
 const Achievements = () => {
     return (
@@ -8,8 +9,8 @@ const Achievements = () => {
                     <h4>6 stars on <span>hackerrank</span> (Problem Solving)</h4>
                     <h4>4 stars on <span>Codechef</span></h4>
                     <h4>Pupil (1220) at <span>codeforces</span></h4>
-                    <h4>React js certificattion <span>Hackerrank</span></h4>
-                    <h4>Google Hash Code 2021 (qualification certificate)</h4>
+                    <h4>React js certificattion <span>Hackerrank</span> <LinkIcon className="certificate-links"/></h4>
+                    <h4>Google Hash Code 2021 (qualification certificate) <LinkIcon className="certificate-links"/></h4>
             </div>
         </div>
         </div>
